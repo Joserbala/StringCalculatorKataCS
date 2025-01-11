113364037d24b3498ef81a4f461f60fd27df6519
Joser Gala Naranjo <jgalanar@gmail.com>
2025-01-11 03:34:31 +0100

    test: rename test methods

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -2,28 +2,28 @@
 
 namespace Tests;
 
-public class Tests
+public class StringCalculatorTests
 {
 	[Test]
-	public void CalculatorReturnsZeroWhenEmptyParameters()
+	public void Empty_string_sums_0()
 	{
 		StringCalculator.Add("").Should().Be(0);
 	}
 
 	[Test]
-	public void ReturnsTheSameNumberWhenProvidingOneNumber()
+	public void Single_number_sum()
 	{
 		StringCalculator.Add("1").Should().Be(1);
 	}
 
 	[Test]
-	public void ReturnsTheSumOfTwoNumbersWhenProvidingTwoNumbers()
+	public void Two_numbers_sum()
 	{
 		StringCalculator.Add("2,1").Should().Be(3);
 	}
 
 	[Test]
-	public void AddingNineNumbersReturnsTheSumOfNineNumbers()
+	public void Nine_numbers_sum()
 	{
 		StringCalculator.Add("1,2,3,4,5,6,7,8,9").Should().Be(45);
 	}
